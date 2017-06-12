4a4924583ea4a65776e2a08b74054d1fc28fbf56
Pablo <pabloguaza@gmail.com>
2017-06-12 13:09:54 +0200

    Add testing DB config

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -12,24 +12,24 @@ class Welcome_test extends TestCase
 {
 	public function test_index()
 	{
-		$output = $this->request('GET', 'welcome/index');
-		$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
+		$output = $this->request('GET', '/');
+		$this->assertContains('CAFPE', $output);
 	}
 
-	public function test_method_404()
-	{
-		$this->request('GET', 'welcome/method_not_exist');
-		$this->assertResponseCode(404);
-	}
-
-	public function test_APPPATH()
-	{
-		$actual = realpath(APPPATH);
-		$expected = realpath(__DIR__ . '/../..');
-		$this->assertEquals(
-			$expected,
-			$actual,
-			'Your APPPATH seems to be wrong. Check your $application_folder in tests/Bootstrap.php'
-		);
-	}
+	// public function test_method_404()
+	// {
+	// 	$this->request('GET', 'welcome/method_not_exist');
+	// 	$this->assertResponseCode(404);
+	// }
+    //
+	// public function test_APPPATH()
+	// {
+	// 	$actual = realpath(APPPATH);
+	// 	$expected = realpath(__DIR__ . '/../..');
+	// 	$this->assertEquals(
+	// 		$expected,
+	// 		$actual,
+	// 		'Your APPPATH seems to be wrong. Check your $application_folder in tests/Bootstrap.php'
+	// 	);
+	// }
 }
