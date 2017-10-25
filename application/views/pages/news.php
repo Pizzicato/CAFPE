c1e79caeb9aee324ed0180d6f480cce681f391a8
Pablo <pabloguaza@gmail.com>
2017-10-25 13:41:02 +0200

    Improve new public page

--- application/views/pages/news.php
+++ application/views/pages/news.php
@@ -1,13 +1,29 @@
-<h3>All news</h3>
-<table>
-{articles}
+<h3><?= lang('news') ?></h3><br>
+<?php if ($articles): ?>
+<table class="table table-striped table-hover">
+    <thead>
     <tr>
-        <td>{date}</td>
-        <td>{title}</td>
-        <td>{content}</td>
-        <td>{main_pic}</td>
-        <td>
-            <a href="<?= site_url_lang('article') ?>/{slug}">view</a></td>
+        <th><?= lang('date') ?></th>
+        <th><?= lang('title') ?></th>
+        <th></th>
     </tr>
-{/articles}
+    </thead>
+    <?php foreach ($articles as $_) : ?>
+    <tr>
+        <td class="text-nowrap"><?= $_['date'] ?></td>
+        <td><?= $_['title'] ?></td>
+        <td class="text-right text-nowrap">
+            <?=
+                anchor(
+                    site_url_lang('article/'.$_['slug']),
+                    '<i data-toggle="tooltip" title="'.lang('view').'" class="fa fa-eye" aria-hidden="true"></i>',
+                    array('class' => 'action-icon')
+                )
+            ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
 </table>
+<?php else: ?>
+    <p><?= lang('no_records') ?></p>
+<?php endif; ?>
